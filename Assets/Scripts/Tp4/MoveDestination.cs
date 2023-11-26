17e17b231341e6312381ac483f9566e301e30360
m v <morgane.voisin@e-artsup.net>
2023-11-26 11:10:07 +0100

    avancé tp4

--- Assets/Scripts/Tp4/MoveDestination.cs
+++ Assets/Scripts/Tp4/MoveDestination.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Tp4 : MonoBehaviour
+public class MoveDestination : MonoBehaviour
 {
+
+    public Transform goal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
     }
 
     // Update is called once per frame
